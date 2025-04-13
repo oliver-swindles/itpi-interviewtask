52cdb06b6d3ee94bea758c7c7f71c19d9f18476a
Oliver Swindles <86194326+oliver-swindles@users.noreply.github.com>
2025-04-13 10:32:27 +0100

    Added navbar

---
+++ src/lib/components/shared/Header.svelte
@@ -0,0 +1,13 @@
+<script>
+  import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
+</script>
+
+<Navbar class="shadow-md">
+  <NavBrand href="/">
+    <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">LU CAREERS</span>
+  </NavBrand>
+  <NavHamburger  />
+  <NavUl >
+    <NavLi href="https://github.com/oliver-swindles/itpi-interviewtask" target="_blank">Project's GitHub</NavLi>
+  </NavUl>
+</Navbar>
